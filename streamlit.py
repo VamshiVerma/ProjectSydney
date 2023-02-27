d621dfde18b2f4f49e4881dc58bbb70aabc76325
Vamshi Verma <vamshipaili@outlook.com>
2023-02-27 12:24:48 -0500

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import openai
+
+# Replace FINE_TUNED_MODEL with the name of your fine-tuned model
+model_name = "davinci:ft-personal-2023-02-24-03-55-15"
+
+openai.api_key ="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"
+
+def on_submit(prompt):
+    # Make the completion request
+    completion = openai.Completion.create(model=model_name, prompt=prompt)
+
+    # Get the completion text from the first choice in the choices list
+    text = completion.choices[0]["text"]
+
+    return text
+
+def main():
+    # Create the main page title
+    st.title("Fine-tuned GPT-3")
+
+    # Create the input field
+    prompt = st.text_input("Enter prompt:")
+
+    # Create the submit button and display the result
+    if st.button("Submit"):
+        result = on_submit(prompt)
+        st.write(result)
+
+if __name__ == "__main__":
+    main()
